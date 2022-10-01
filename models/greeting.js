50d12ebabb477bcc091ffe5df7a3085fe99f2c26
OliveOyl89 <ashleynickens@gmail.com>
2022-10-01 17:09:35 -0400

    Final Commit

---
+++ models/greeting.js
@@ -0,0 +1,18 @@
+// const greeting = ['Hello', 'What\'s up']
+
+// const names = [
+//     {
+//         name: 'Ashley'
+//     },
+//     {
+//         name: 'Arthur'
+//     },
+//     {
+//         name: 'Kisa'
+//     }
+// ]
+
+// module.exports = {
+//     greeting,
+//     names
+// }
--- server.js
+++ server.js
@@ -1,18 +1,54 @@
 const express = require('express')
 const app = express()
 
-// Greetings
 
+
+// Greetings
 app.get('/greeting', (req, res) => {
-    res.status(200).json({ msg: 'Hello stranger' })
+    res.status(200).send(`<h1>Hello stranger</h1>`)
 })
 
+app.get('/greeting/:name', (req, res) => {
+    res.status(200).send(`<h1>${req.params.name}</h1>`)
+})
+
+
 // Tip Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.status(200).send(`<h1>${req.params.total.tipPercentage}</h1>`)
+})
 
+ // Magic 8 Ball
+app.get('/magic/:question', (req, res) => {
+    const phrases = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it yes", 
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    ]
 
-// Magic 8 Ball
+    const randomNum = Math.floor(Math.random() * phrases.length)
+    res.status(200).send(`<h1>${req.params.question}</h1> <p>${phrases[randomNum]}</p>`)
+})
+
 
 
 app.listen(3000, () => {
-    console.log('Ashley is listening')
+    console.log('I am listening on port 3000')
 })
